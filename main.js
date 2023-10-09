26b75526b0e7163d4dce5f5579f250bc7bb25d56
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-10-09 13:27:52 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+arr = [];
+
+
+function setup() {
+  canvas = createCanvas(400, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R3rBWMPIl/model.json", modelloaded);
+}
+
+function modelloaded(){
+  console.log("model loaded");
+}
+
+
+function draw(){
+ image(video, 0,0, 300, 300);
+ classifier.classify(video, gotResult);
+}
+
+previous_result = "";
+
+function gotResult(error, results){
+  if (error) {
+    console.log(error);
+  } else {
+    
+    if ((results[0].confidence>0.5)&& previous_result != results[0].label) {
+      console.log(results);
+      previous_result = results[0].label;
+      synth = window.speechSynthesis
+      speakdata = "object detected is " + results[0].label;
+      utterthis = new SpeechSynthesisUtterance(speakdata);
+      synth.speak(utterthis);
+      document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence*100).toFixed(2);
+      
+    }
+  }
+}
+
+var sentence = [];
+
+function printDaArray() {
+  console.log(previous_result);
+  //document.getElementById("textarea1").innerHTML = previous_result;
+  sentence = sentence.concat(previous_result);
+  document.getElementById("textarea1").innerHTML = sentence.join("");
+}
+
+printDaArray()
+
+let startTimer = setInterval(printDaArray, 2000);
+
+// setTimeout(() => {
+//   arr.push(results[0].label);
+//   console.log("you're 2 idiots")
+// }, 2000);
+
+
+
+
+
+
+
