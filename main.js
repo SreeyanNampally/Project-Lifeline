c49e0a8be4400d248388b05b5dcab7540df97647
Sreeyan Nampally <98562308+SreeyanNampally@users.noreply.github.com>
2023-10-19 17:15:50 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -5,44 +5,47 @@ switchFlag = false;
 
 
 function setup() {
-  canvas = createCanvas(300, 400);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R3rBWMPIl/model.json", modelloaded);
+     canvas = createCanvas(300, 400);
+     var x = (window.innerWidth - 300) / 2
+     var y = (window.innerWidth - 400) / 2
+     canvas.position(x, 100);
+     canvas.center();
+     video = createCapture(VIDEO);
+     video.hide();
+     classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/R3rBWMPIl/model.json", modelloaded);
 
 }
 
 
-function modelloaded(){
-  console.log("model loaded");
+function modelloaded() {
+     console.log("model loaded");
 }
 
 
-function draw(){
- image(video, 0,0, 300, 400);
- classifier.classify(video, gotResult);
+function draw() {
+     image(video, 0, 0, 300, 400);
+     classifier.classify(video, gotResult);
 }
 
 previous_result = "";
 
-function gotResult(error, results){
-  if (error) {
-    console.log(error);
-  } else {
-    
-    if ((results[0].confidence>0.5)&& previous_result != results[0].label) {
-      console.log(results);
-      previous_result = results[0].label;
-      //synth = window.speechSynthesis
-      speakdata = "object detected is " + results[0].label;
-      //utterthis = new SpeechSynthesisUtterance(speakdata);
-      //synth.speak(utterthis);
-      document.getElementById("result_object_name").innerHTML = results[0].label;
-      document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence*100).toFixed(2);
-      
-    }
-  }
+function gotResult(error, results) {
+     if (error) {
+          console.log(error);
+     } else {
+
+          if ((results[0].confidence > 0.5) && previous_result != results[0].label) {
+               console.log(results);
+               previous_result = results[0].label;
+               //synth = window.speechSynthesis
+               speakdata = "object detected is " + results[0].label;
+               //utterthis = new SpeechSynthesisUtterance(speakdata);
+               //synth.speak(utterthis);
+               document.getElementById("result_object_name").innerHTML = results[0].label;
+               document.getElementById("result_object_accuracy").innerHTML = (results[0].confidence * 100).toFixed(2);
+
+          }
+     }
 }
 
 acc_result = previous_result
@@ -50,10 +53,10 @@ acc_result = previous_result
 var sentence = [];
 
 function printDaArray() {
-    console.log(previous_result);
-    sentence = sentence.concat(previous_result);
-    document.getElementById("textarea1").innerHTML = sentence.join("");
-  
+     console.log(previous_result);
+     sentence = sentence.concat(previous_result);
+     document.getElementById("textarea1").innerHTML = sentence.join("");
+
 }
 
 
@@ -61,28 +64,26 @@ let startTimer = setInterval(printDaArray, 2000);
 printDaArray()
 
 
-function switchcam(){
-    switchFlag = !switchFlag;
-      if(switchFlag == true){
-        VIDEO = {
-          video: {
-            facingMode: {
-            exact: "environment"
-          }
-        }
-      };
-      }
-      else{
-        VIDEO = {
-          video: {
-              facingMode: {
-              exact: "user"
-            }
-          }
-        };
-      }
-      setup();
-  //capture = createCapture(VIDEO);
-  //video.hide();
+function switchcam() {
+     switchFlag = !switchFlag;
+     if (switchFlag == true) {
+          VIDEO = {
+               video: {
+                    facingMode: {
+                         exact: "environment"
+                    }
+               }
+          };
+     } else {
+          VIDEO = {
+               video: {
+                    facingMode: {
+                         exact: "user"
+                    }
+               }
+          };
+     }
+     setup();
+     //capture = createCapture(VIDEO);
+     //video.hide();
 }
-
